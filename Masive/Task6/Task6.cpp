79073522012c59cdc411a88373bc433b7b45e5d7
asya-korobka <117941992+asya-korobka@users.noreply.github.com>
2022-12-04 23:07:25 +0200

    Update Task6.cpp

--- Masive/Task6/Task6.cpp
+++ Masive/Task6/Task6.cpp
@@ -1,7 +1,7 @@
 // Task6.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-//Задача 6 на одновимірні масиви
+//Р—Р°РґР°С‡Р° 6 РЅР° РѕРґРЅРѕРІРёРјС–СЂРЅС– РјР°СЃРёРІРё
 
 #include <iostream>
 #include <math.h>
